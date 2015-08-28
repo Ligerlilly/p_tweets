7b79af625b40c672a17bd91b017ed03e2edf3f43
Jason <quizathaderat@yahoo.com>
2015-08-28 15:24:36 -0700

    Add search form to index route

--- twitter_fetcher.rb
+++ twitter_fetcher.rb
@@ -16,10 +16,20 @@ class TwitterFetcher < Sinatra::Base
     :oauth_token_secret => ENV['oath_token_secret'],
   )
 
+  @query = ''
+
   get '/' do
+     erb :index
+  end
+
+  post '/search' do
+    redirect "/search/#{params['query']}"
+  end
+
+  get '/search/:query' do
     @tweets = []
-    @@twitter_client.search('#portland', result_type: 'recent').results.map do |tweet|
-      @tweets.push "#{tweet.user.screen_name}: #{tweet.text}"
+    @@twitter_client.search("#{params['query']}", {result_type: 'recent', geocode: "45.5434085,-122.654422,8mi", count: 50}).results.map do |tweet|
+      @tweets.push "#{tweet.user.screen_name}: #{tweet.text} **** #{tweet.user.location}"
     end
     erb :tweets
   end
