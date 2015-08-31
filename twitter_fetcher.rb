5007f8b44b7bc054fc3e4c6488adae27811ff61d
Jason <quizathaderat@yahoo.com>
2015-08-30 22:39:44 -0700

    Add multi word searches

--- twitter_fetcher.rb
+++ twitter_fetcher.rb
@@ -16,25 +16,32 @@ class TwitterFetcher < Sinatra::Base
     :oauth_token_secret => ENV['oath_token_secret'],
   )
 
-  @query = ''
+
 
   get '/' do
      erb :index
   end
 
   post '/search' do
-    redirect "/search/#{params['query']}"
+    query = params['query']
+    if query.include?(' ')
+      query.gsub!(' ', '+')
+    end
+
+    redirect "/search/#{query}"
   end
 
+
   get '/search/:query' do
+    query = params['query']
+    if query.include?('+')
+      query.gsub!('+', ' ')
+    end
     @tweets = []
-    @@twitter_client.search("#{params['query']}", {result_type: 'recent', geocode: "45.5434085,-122.654422,8mi", count: 50}).results.map do |tweet|
-      
+    @@twitter_client.search("#{query}", {result_type: 'recent', geocode: "45.5434085,-122.654422,8mi", count: 50}).results.map do |tweet|
       @tweets.push "<img src='#{tweet.user.profile_image_url}' alt='img'> #{tweet.user.screen_name}: #{tweet.text} **** #{tweet.user.location}"
     end
     erb :tweets
   end
-
-
 
 end
